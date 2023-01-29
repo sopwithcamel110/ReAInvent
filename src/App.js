43708d294623c8a12ed1987301825f911d20d09c
Donovan deVise <sopwithcamel110@gmail.com>
2023-01-29 13:59:57 -0800

    Created react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React , { useRef, useState } from 'react';
 
 function App() {
+  const linkInputRef = useRef();
+  const [ chatBox, updateChatBox ] = useState();
+  async function handleAnalyzeClick() {
+    let linkInput = linkInputRef.current.value;
+    
+    chat(linkInput);
+  }
+  async function chat(url) {
+    // link: video url
+    // PROCESS LINK HERE
+    updateChatBox(url);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h3>Enter a video link below:</h3>
+    <input type="text" ref={linkInputRef}/>
+    <input type="button" onClick={handleAnalyzeClick} value="Analyze"/>
+    <hr/>
+    <div>
+      {chatBox}
     </div>
+    </>
   );
 }
 
