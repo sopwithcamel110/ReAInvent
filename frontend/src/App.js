5a8d736d391978753bdbf731484941cb8a149755
Donovan deVise <ddevise@ucsc.edu>
2023-02-16 19:49:52 -0800

    Created separate file for chatbox element, hidden until video is loaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import React, { useRef, useState } from "react";
 import ReactPlayer from 'react-player'
 import "./App.css";
-import QandA from './QandA.js';
-import { FiSend } from 'react-icons/fi';
-import {GrYoutube} from 'react-icons/gr';
+import Chatbox from './Chatbox.js';
+import { GrYoutube } from 'react-icons/gr';
 import { AtomLoader } from "react-loaders-kit";
 
 function App() {
   const API_ENDPOINT = "/api";
   const urlInputRef = useRef();
-  const questionRef = useRef();
   const playerRef = useRef();
   const [url, setUrl] = useState();
   const [progress, setProgress] = useState();
-  const [chat, setChat] = useState([]);
   const [transcriptLoader, showTranscriptLoader] = useState(false);
+  const [chatbox, showChatbox] = useState(false);
   const loaderProps = {
     loading: true,
     size: 80,
@@ -22,30 +20,6 @@ function App() {
     colors: ["#FF2900", "#FF5500", "#FFE600"],
   };
   let desc = "None";
-
-  function handleQuestionEnter(e) {
-    if (e.key === 'Enter') {
-      askQuestion();
-    }
-  }
-  
-  function askQuestion() {
-    // Ask question
-    let text = questionRef.current.value;
-    questionRef.current.value = "";
-    setChat(oldArray => [["Question", text, []], ...oldArray]);
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ question: text })
-    };
-    fetch(API_ENDPOINT + '/ask', requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.stamps);
-        setChat(oldArray => [["Answer", data.answer, data.stamps], ...oldArray]);
-      });
-  }
 
   function CheckServerStatus() {
     setProgress("Checking server status...");
@@ -102,7 +76,7 @@ function App() {
       if (data.Completed === 1) {
         console.log("Transcript loaded.");
         setUrl("https://www.youtube.com/watch?v=" + desc)
-        setChat([]);
+        showChatbox(true);
         playerRef.current.scrollIntoView({behavior: "smooth"});
         setProgress("");
         showTranscriptLoader(false);
@@ -140,23 +114,9 @@ function App() {
       <div ref={playerRef}>
         <ReactPlayer controls={true} url={url} id="videoplayer"/>
       </div>
-      <div className="chatbox">
-        <div className="questionInput">
-          <input id="questionTextInput" type="text" ref={questionRef} onKeyDown={handleQuestionEnter}/>
-          <FiSend className="questionButton" onClick={askQuestion} size= "20px" style= {{
-            marginTop: "10px"
-          }}/>
-        </div>
-        <div className="QandA">
-          {
-            chat.map((value) => {
-              return (
-                <QandA playerRef={playerRef} type={value[0]} text={value[1]} stamps={value[2]}/>
-              )
-            })
-          }
-        </div>
-      </div>
+      {
+        chatbox ? <Chatbox playerRef={playerRef} endpoint={API_ENDPOINT}/> : <div/>
+      }
 		</>
 	);
 }
---
+++ frontend/src/Chatbox.js
@@ -0,0 +1,52 @@
+import React, { useRef, useState } from "react";
+import QandA from './QandA.js';
+import { FiSend } from 'react-icons/fi';
+import "./Chatbox.css";
+
+function Chatbox(args) {
+    const questionRef = useRef();
+    const [chat, setChat] = useState([]);
+    function handleQuestionEnter(e) {
+        if (e.key === 'Enter') {
+          askQuestion();
+        }
+    }
+    function askQuestion() {
+        // Ask question
+        let text = questionRef.current.value;
+        questionRef.current.value = "";
+        setChat(oldArray => [["Question", text, []], ...oldArray]);
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ question: text })
+        };
+        fetch(args.endpoint + '/ask', requestOptions)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data.stamps);
+            setChat(oldArray => [["Answer", data.answer, data.stamps], ...oldArray]);
+          });
+    }
+	return (
+		<div className="chatbox">
+            <div className="questionInput">
+                <input id="questionTextInput" type="text" ref={questionRef} onKeyDown={handleQuestionEnter}/>
+                <FiSend className="questionButton" onClick={askQuestion} size= "20px" style= {{
+                    marginTop: "10px"
+                }}/>
+            </div>
+            <div className="QandA">
+                {
+                    chat.map((value) => {
+                    return (
+                        <QandA playerRef={args.playerRef} type={value[0]} text={value[1]} stamps={value[2]}/>
+                    )
+                    })
+                }
+            </div>
+      </div>
+	);
+}
+
+export default Chatbox;
